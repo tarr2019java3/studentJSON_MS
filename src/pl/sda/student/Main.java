a8074cac3304276ee4232c11c440752d9e80b2a2
matisarnowski <matisarnowski88@gmail.com>
2020-02-29 11:45:44 +0100

    5 nowych obiektów .json

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -9,11 +9,21 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Student student = new Student("Jan", "Kowalski", 32);
+        Student student1 = new Student("Paweł", "Nowak", 33);
+        Student student2 = new Student("Igor", "Przybylski", 34);
+        Student student3 = new Student("Mateusz", "Sarnowski", 35);
+        Student student4 = new Student("Jakub", "Pawlak", 36);
+        Student student5 = new Student("Maciej", "Gargulec", 37);
+        
+
 
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            objectMapper.writeValue(new File("student.json"), student);
+            objectMapper.writeValue(new File("student1.json"), student1);
+            objectMapper.writeValue(new File("student2.json"), student2);
+            objectMapper.writeValue(new File("student3.json"), student3);
+            objectMapper.writeValue(new File("student4.json"), student4);
+            objectMapper.writeValue(new File("student5.json"), student5);
         } catch (IOException e) {
             e.printStackTrace();
         }
