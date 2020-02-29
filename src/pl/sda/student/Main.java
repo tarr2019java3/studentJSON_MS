91b1152dfee9e9529185ac4e1aa5aea61b701de5
matisarnowski <matisarnowski88@gmail.com>
2020-02-29 11:36:31 +0100

    zapisywanie obiektu do json

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -1,8 +1,21 @@
 package pl.sda.student;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Student student = new Student("Jan", "Kowalski", 32);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File("student.json"), student);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/pl/sda/student/Student.java
@@ -0,0 +1,40 @@
+package pl.sda.student;
+
+public class Student {
+    private String name;
+    private String lastname;
+    private Integer index;
+
+    public Student(String name, String lastname, Integer index) {
+        this.name = name;
+        this.lastname = lastname;
+        this.index = index;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+}
